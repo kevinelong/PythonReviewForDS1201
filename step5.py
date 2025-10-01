724c3806a95ad6dbd3cbfe56e4f26c75b670ee92
Kevin Ernest Long <kevinelong@gmail.com>
2025-10-01 14:12:23 -0700

    totals

--- step5.py
+++ step5.py
@@ -1,19 +1,27 @@
 #demo for loop
-for i in range(5): #five times? 0,1,2,3,4 but not 5
-    print(i)
+# for i in range(5): #five times? 0,1,2,3,4 but not 5
+#     print(i)
 
 
 import random
-r = random.randint(1, 100)
-print(r)
+# r = random.randint(1, 100)
+# print(r)
 
 def roll(sides=6):
     return random.randint(1, sides)
 
-print(roll())
-print(roll())
-print(roll())
+# print(roll())
+# print(roll())
+# print(roll())
 
 def roll_many(n): # n how many dice n for number
+    total = 0
     #how can we repeat in python
-    pass 
+    for _ in range(n): # _ is just a var name like x or i but indicates its not used
+        r = roll()
+        print(r)
+        total += r
+    return total
+
+grand_total = roll_many(5) # roll five dice
+print(grand_total)
